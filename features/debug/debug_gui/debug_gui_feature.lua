8e697b922bc46e19274f03e11318335092252ed8
d954mas <d954mas@gmail.com>
2025-12-18 00:12:35 +0500

    wip

--- features/debug/debug_gui/debug_gui_feature.lua
+++ features/debug/debug_gui/debug_gui_feature.lua
@@ -1,9 +1,17 @@
-
+local DebugGuiStoragePart = require "features.debug.debug_gui.debug_gui_storage_part"
+local STORAGE = require "features.core.storage.storage"
 ---@class DebugGuiFeature:Feature
 local DebugGuiFeature = {}
 
 function DebugGuiFeature:on_liveupdate_loaded()
     collectionfactory.create("liveupdate:/root#debug")
+end
+
+function DebugGuiFeature:on_storage_init()
+    self.storage = DebugGuiStoragePart.new(STORAGE)
+    ---@class Storage
+    local storage = STORAGE
+    storage.debug_gui_storage = self.storage
 end
 
 return DebugGuiFeature
---
+++ features/debug/debug_gui/debug_gui_storage_part.lua
@@ -0,0 +1,34 @@
+local CLASS = require "libs.class"
+
+local StoragePart = require "features.core.storage.storage_part"
+
+---@class DebugStoragePart:StoragePart
+local Storage = CLASS.class("DebugStoragePart", StoragePart)
+
+function Storage.new(...) return CLASS.new_instance(Storage, ...) end
+
+function Storage:initialize(...)
+	StoragePart.initialize(self, ...)
+	self.debug_gui = self.storage.data.debug_gui
+    if not self.debug_gui then
+		self.debug_gui = {
+			show = false,
+		}
+	end
+    ---@class Storage
+    local storage = self.storage
+    storage.debug_gui_storage = self
+end
+
+function Storage:is_show()
+	return self.debug_gui.show
+end
+
+
+function Storage:set_show()
+	self.debug_gui.show = true
+    self:save()
+end
+
+
+return Storage
